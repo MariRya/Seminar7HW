ec22503626f453b632efd257bd2ba725b7539751
Mari Ryazantseva <marirya95@yandex.ru>
2024-05-04 07:23:37 +0300

    Решение 3 задачи

---
+++ Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+
+    }
+    Console.Write("]");
+}
+
+void PrintReverse(int[] array, int i)
+{
+    if (i < 0) return;
+    Console.Write(array[i] + " ");
+    PrintReverse(array, i - 1);
+}
+
+int[] arr = CreateArrayRndInt(6, 1, 35);
+PrintArray(arr);
+Console.WriteLine();
+PrintReverse(arr, arr.Length - 1);
